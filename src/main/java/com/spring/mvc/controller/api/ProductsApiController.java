48d2a75db7e1b413dc61304e908179c79b61b7aa
Jimuel Palaca <jimuel.palaca@whitecloak.com>
2019-09-03 14:04:13 +0800

    Add API Rest Controller

---
+++ src/main/java/com/spring/mvc/controller/api/ProductsApiController.java
@@ -0,0 +1,21 @@
+package com.spring.mvc.controller.api;
+
+import com.spring.mvc.model.Product;
+import com.spring.mvc.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductsApiController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/api/products")
+    public List<Product> index() {
+        return productService.get();
+    }
+}
